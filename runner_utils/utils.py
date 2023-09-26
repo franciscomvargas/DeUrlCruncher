806627e707a262839e424d56d9beb443ef4093c9
franciscomvargas <franciscomvargas24@gmail.com>
2023-09-26 02:19:36 +0100

    Runner Utils - bugfix + clean

--- runner_utils/utils.py
+++ runner_utils/utils.py
@@ -4,22 +4,45 @@ import requests
 import re
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
-API_UP =  "http://129.152.27.36/assistant/api_uploads"
+API_UP =  "https://desota.net/assistant/api_uploads"
 
-# QUESTION-ANSWER MODEL
-def parse_text(desota_arg):
+# UTILS
+def fix_file_name(file_name):
     # TEMPORARY FIX
     # retrieved from https://stackoverflow.com/a/3642850 & https://stackoverflow.com/a/32680048
     _file_pattern = re.compile(r'file\(((.)*)\)')
-    _file_res = _file_pattern.search(desota_arg)
+    _file_res = _file_pattern.search(file_name)
     if _file_res:
-        desota_arg = _file_res.group(1)
+        file_name = _file_res.group(1)
     _text_pattern = re.compile(r'text\(((.)*)\)')
-    _text_res = _text_pattern.search(desota_arg)
+    _text_res = _text_pattern.search(file_name)
     if _text_res:
-        desota_arg = _text_res.group(1)
+        file_name = _text_res.group(1)
+    return file_name
+
+def get_file_content(file_name):
+    file_name = fix_file_name(file_name)
+    file_url =f'{API_UP}/{file_name}'
+    
+    file_content = ""
+    with  requests.get(file_url, stream=True) as req_file:
+        if req_file.status_code != 200:
+            return None
+        
+        if req_file.encoding is None:
+            req_file.encoding = 'utf-8'
+
+        for line in req_file.iter_lines(decode_unicode=True):
+            if line:
+                file_content += line
+    return file_content
 
 
+# MODELS
+#   > QUESTION-ANSWER
+def parse_text(desota_arg):
+    desota_arg = fix_file_name(desota_arg)
+    
     if desota_arg.endswith(".txt"):
         download_desota_url = f'{API_UP}/{desota_arg}'
         base_filename = os.path.basename(desota_arg)
@@ -51,14 +74,14 @@ def parse_text(desota_arg):
     
 def get_request_qa(model_request_dict):
     _context, _question = None, None
-    if "context" in model_request_dict["input_args"]:
+    if "context" in model_request_dict["input_args"] and "question" in model_request_dict["input_args"]:
         _context = parse_text(model_request_dict["input_args"]["context"])
-    if "question" in model_request_dict["input_args"]:
         _question = parse_text(model_request_dict["input_args"]["question"])
 
     return _context, _question
+    
 
-# URL MODEL
+#   > URL
 def get_url_from_str(string):
     # retrieved from https://www.geeksforgeeks.org/python-check-url-string/
     # findall() has been used
@@ -67,26 +90,29 @@ def get_url_from_str(string):
     url = re.findall(regex, string)
     return [x[0] for x in url]
 
+def get_url_from_file(file_name):
+    file_content = get_file_content(file_name)
+    return get_url_from_str(file_content)
+
 def get_request_url(model_request_dict):
     _req_url = None
     if 'url' in model_request_dict["input_args"]:
         _req_url = get_url_from_str(model_request_dict["input_args"]['url'])
-    
+        if not _req_url:
+            _req_url = get_url_from_file(model_request_dict["input_args"]['url'])
+            
+    if not _req_url and 'file' in model_request_dict["input_args"] and "file_name" in model_request_dict["input_args"]["file"]:
+        _req_url = get_url_from_file(model_request_dict["input_args"]['file']['file_name'])
+
     if not _req_url and 'text_prompt' in model_request_dict["input_args"]:
         _req_url = get_url_from_str(model_request_dict["input_args"]['text_prompt'])
-
-    if not _req_url and 'file' in model_request_dict["input_args"]:
-        if isinstance(model_request_dict["input_args"]["file"], str):
-            _req_url = get_url_from_str(model_request_dict["input_args"]['file'])
-        elif isinstance(model_request_dict["input_args"]["file"], list):
-            for file in model_request_dict["input_args"]["file"]:
-                _req_url = get_url_from_str(file)
-                if _req_url:
-                    break
+        if not _req_url:
+            _req_url = get_url_from_file(model_request_dict["input_args"]['text_prompt'])
+    
     return _req_url
 
 
-# HTML MODEL
+#   > HTML
 def get_html_from_str(string):
     # retrieved from https://stackoverflow.com/a/3642850 & https://stackoverflow.com/a/32680048
     pattern = re.compile(r'<html((.|[\n\r])*)\/html>')
@@ -100,7 +126,10 @@ def get_html_from_str(string):
         fw.write(_html_content)
     return _tmp_html_path, 'utf-8'
 
-def get_html_from_url(file_url):
+def get_html_from_file(file_name):
+    file_name = fix_file_name(file_name)
+    file_url =f'{API_UP}/{file_name}'
+    
     base_filename = os.path.basename(file_url)
     file_path = os.path.join(CURRENT_PATH, base_filename)
     with  requests.get(file_url, stream=True) as req_file:
@@ -124,26 +153,29 @@ def get_request_html(model_request_dict, from_url=False):
     html_file = None
     html_encoding = None
     if from_url and 'url' in model_request_dict["input_args"]:
-        if model_request_dict["input_args"]["url"].endswith(".html"):
-            html_file, html_encoding = get_html_from_url(f'{API_UP}/{model_request_dict["input_args"]["url"]}')
+        fix_arg = fix_file_name(model_request_dict["input_args"]["url"])
+        if fix_arg.endswith(".html"):
+            html_file, html_encoding = get_html_from_file(fix_arg)
         else:
             html_file, html_encoding = get_html_from_str(model_request_dict["input_args"]["url"])
 
     if not html_file and 'html' in model_request_dict["input_args"]:
-        if model_request_dict["input_args"]["html"].endswith(".html"):
-            html_file, html_encoding = get_html_from_url(f'{API_UP}/{model_request_dict["input_args"]["html"]}')
+        fix_arg = fix_file_name(model_request_dict["input_args"]["html"])
+        if fix_arg.endswith(".html"):
+            html_file, html_encoding = get_html_from_file(fix_arg)
         else:
             html_file, html_encoding = get_html_from_str(model_request_dict["input_args"]["html"])
 
-    if not html_file and 'file' in model_request_dict["input_args"] and "file_name" in model_request_dict["input_args"]["file"] and model_request_dict["input_args"]["file"]["file_name"].endswith(".html") and "file_url" in model_request_dict["input_args"]["file"]:
-            html_file, html_encoding = get_html_from_url(model_request_dict["input_args"]["file"]["file_url"])
+    if not html_file and 'file' in model_request_dict["input_args"] and "file_name" in model_request_dict["input_args"]["file"]:
+        html_file, html_encoding = get_html_from_file(model_request_dict["input_args"]["file"]["file_name"])
             
     if not html_file and 'text_prompt' in model_request_dict["input_args"]:
         html_file, html_encoding = get_html_from_str(model_request_dict["input_args"]['text_prompt'])
         
     return html_file, html_encoding
 
-# TEXT MODEL
+
+#   > TEXT
 def get_request_text(model_request_dict):
     _req_text = None
     if 'query' in model_request_dict["input_args"]:
@@ -151,12 +183,8 @@ def get_request_text(model_request_dict):
     
     if 'text_prompt' in model_request_dict["input_args"]:
         _req_text = parse_text(model_request_dict["input_args"]['text_prompt'])
+    
+    if not _req_text and 'file' in model_request_dict["input_args"] and "file_name" in model_request_dict["input_args"]["file"]:
+        _req_text = get_file_content(model_request_dict["input_args"]['file']['file_name'])
 
-    if not _req_text and 'file' in model_request_dict["input_args"]:
-        if isinstance(model_request_dict["input_args"]["file"], str):
-            _req_text = parse_text(model_request_dict["input_args"]['file'])
-        elif isinstance(model_request_dict["input_args"]["file"], list):
-            _req_text = ""
-            for file in model_request_dict["input_args"]["file"]:
-                _req_text += f"{parse_text(file)}\n"
     return _req_text
